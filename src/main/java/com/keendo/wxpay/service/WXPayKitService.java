b24b67f623549d1ea9f45213d4f4ae56d7f7b2ca
xiaxin <244011876@qq.com>
2018-10-11 15:51:00 +0800

    支付回调

--- src/main/java/com/keendo/wxpay/service/WXPayKitService.java
+++ src/main/java/com/keendo/wxpay/service/WXPayKitService.java
@@ -1,20 +1,31 @@
 package com.keendo.wxpay.service;
 
-import com.keendo.wxpay.bean.OrderQueryReq;
-import com.keendo.wxpay.bean.OrderQueryResp;
-import com.keendo.wxpay.bean.UnifiedorderReq;
-import com.keendo.wxpay.bean.UnifiedorderResp;
+import com.keendo.wxpay.bean.*;
 import com.keendo.wxpay.constant.WXPayConstants;
 import com.keendo.wxpay.exception.BizException;
 import com.keendo.wxpay.utils.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.math.BigDecimal;
 
 @Service
 public class WXPayKitService {
 
 
+    @Autowired
+    IPayResultService payResultService;//对支付接口进行处理的方法
+
+    @Autowired
+    private PayRecordService payRecordService;
+
     //查询订单url
     private static String QUERY_ORDER_URL = "https://api.mch.weixin.qq.com/pay/orderquery";
+
+    private static final String RESULT_CODE_SUCCESS = "SUCCESS";
 
 
     /**
@@ -68,6 +79,82 @@ public class WXPayKitService {
             throw new BizException("系统繁忙,请稍后再试");
         }
 
+    }
+
+    /**
+     * 支付回调处理方法
+     * @param request
+     * @param response
+     * @throws Exception
+     */
+    @Transactional
+    public void callback(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String xmlRet = WXPayUtil.readXml(request);
+
+        Boolean signValid = WXPayUtil.checkSign(xmlRet);
+
+        String resultCode = WXPayUtil.getResultCode(xmlRet);
+
+        //回调信息正确且验签成功
+        if (signValid && RESULT_CODE_SUCCESS.equals(resultCode)) {
+
+            PayCallBack payCallBack = XmlBeanUtil.toBeanWithCData(xmlRet, PayCallBack.class);
+
+            Integer totalFee = payCallBack.getTotalFee();//回调支付金额
+
+            String orderNo = payCallBack.getOutTradeNo();//回调系统订单号
+
+            String thirdPartOrderNo = payCallBack.getTransactionId();//微信第三方支付流水号
+
+            Boolean isOrderExist = payResultService.isOrderExist(orderNo);
+
+            Boolean isOrderPaid = payResultService.isOrderPaid(orderNo);
+
+            BigDecimal orderFee = payResultService.getOrderFee(orderNo);
+            Boolean isValidFee = totalFee.equals((WXPayUtil.getTotalFee(orderFee)).intValue());
+
+            //订单不存在 || 订单已支付 || 金额不匹配
+            if (!isOrderExist || isOrderPaid || !isValidFee) {
+                fail(response);
+            }
+
+            /*以下为支付成功操作,相同事务域*/
+
+            //修改订单状态
+            payResultService.orderSuccess(orderNo);
+
+            //修改支付记录状态并记录第三方流水号
+            payRecordService.success(orderNo, thirdPartOrderNo);
+
+            //是否有必要把回调信息单独记录下来?
+
+            ok(response);
+
+        } else {
+            fail(response);
+        }
+    }
+
+    /**
+     * 响应微信
+     *
+     * @param response
+     * @throws Exception
+     */
+    private void ok(HttpServletResponse response) throws Exception {
+        String xml = XmlBeanUtil.toXmlWithCData(WXNotifyResp.fail());
+        WXPayUtil.writeXml(response, xml);
+    }
+
+    /**
+     * 响应微信
+     *
+     * @param response
+     * @throws Exception
+     */
+    private void fail(HttpServletResponse response) throws Exception {
+        String xml = XmlBeanUtil.toXmlWithCData(WXNotifyResp.ok());
+        WXPayUtil.writeXml(response, xml);
     }
 
 
