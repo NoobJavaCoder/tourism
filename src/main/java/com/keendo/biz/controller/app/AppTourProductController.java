0bedb56bd633ce3c52c428d1adf5135568a253bd
xiaxin <244011876@qq.com>
2018-10-15 10:01:59 +0800

    appproduct

---
+++ src/main/java/com/keendo/biz/controller/app/AppTourProductController.java
@@ -0,0 +1,10 @@
+package com.keendo.biz.controller.app;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/app/tour-product")
+public class AppTourProductController {
+
+}
---
+++ src/main/java/com/keendo/biz/service/bean/tourproduct/TourProductItem.java
@@ -0,0 +1,16 @@
+package com.keendo.biz.service.bean.tourproduct;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class TourProductItem {
+    /*封面图 标题 行程概要 */
+    private Integer id;
+    private String tourSummary;
+    private String title;
+    private Date deadline;
+    private Integer tourDay;
+    private Date endTime;
+    private BigDecimal price;
+
+}
